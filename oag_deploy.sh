8ce6460d5b3c53781219ee2aecf59701ca249c5c
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-10-21 23:31:44 +0100

    enhance OAG deployment script

--- oag_deploy.sh
+++ oag_deploy.sh
@@ -73,19 +73,58 @@ sudo mkdir sysadmin
 sudo chown cloo:cloo sysadmin/
 git clone https://github.com/CottageLabs/sysadmin.git
 cd sysadmin/
+echo
+echo
 echo "Not replacing your main supervisord configuration file. However, check if it's set up the way you want it."
 echo "Run this to see if there are any differences between the supervisord config in the sysadmin repo and the one on this machine:"
 echo
 echo "    diff -u /etc/supervisor/supervisord.conf /opt/sysadmin/config/supervisor/supervisord.conf"
 echo
+echo
 printf "Copying all supervisord application configuration files... "
 sudo cp -r config/supervisor/conf.d/oag* /etc/supervisor/conf.d/
 echo "done."
+echo
 cd /opt/oag/src/OpenArticleGauge/
 # install lxml
 sudo apt-get update > /dev/null
 echo "Installing lxml, handing off to apt-get now..."
+echo
 sudo apt-get install libxml2-dev libxslt-dev
+echo
 echo "Done trying to install lxml, proceeding with installing Python-based requirements."
+echo
 pip install -e .
+pip install gunicorn
+pip install flower
+echo
+echo
 sudo supervisorctl reload || die "Couldn't reload the supervisord config. Do you have supervisord? Install it if not."
+echo
+sudo supervisorctl status
+cat << EOF
+
+All done. The supervisord status command output you just saw should look
+something like this:
+
+oag                              RUNNING    pid 5958, uptime 0:01:14
+oag-celery                       EXITED     Oct 21 11:24 PM
+oag-celery-flower                RUNNING    pid 5960, uptime 0:01:14
+oag-celerybeat                   RUNNING    pid 5959, uptime 0:01:14
+
+oag-celery should say EXITED, don't worry about it. (If it says RUNNING,
+it will switch to EXITED very soon.)
+
+You can test the whole setup by doing
+
+    curl localhost:5051/lookup/doi:10.1371/journal.pone.0031314.json
+    # This should return a small JSON result, with "requested": 1 in it.
+
+    # Now wait 2 to 30 seconds, give it some time to look it up.
+    curl localhost:5051/lookup/doi:10.1371/journal.pone.0031314.json
+    # This should return a larger JSON result, with lots of info in the
+    # "results" key.
+
+You can also open a browser to http://localhost:5555 to use Flower, the
+Celery monitor. You can see the workers going about their tasks.
+EOF
