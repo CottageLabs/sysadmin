037837ac7c5259bfc18eafc02a0fee76c94b788b
Emanuil Tolev <emanuil.tolev@gmail.com>
2015-02-25 18:43:35 +0000

    update DOAJ CI script

--- doaj/doaj_deploy_staging.sh
+++ doaj/doaj_deploy_staging.sh
@@ -3,9 +3,9 @@ sudo apt-get -q -y install libxml2-dev libxslt-dev python-dev lib32z1-dev
 cd /opt/doaj
 . bin/activate
 cd src/doaj
-git checkout master
+git checkout production
 git pull
 git submodule init
-git submodule update
+git submodule update --recursive
 pip install -r requirements.txt
 sudo supervisorctl restart doaj-staging
