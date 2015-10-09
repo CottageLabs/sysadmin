6178750523d36fd44d7fa578e90e5b1d3afbddae
Emanuil Tolev <emanuil.tolev@gmail.com>
2015-10-09 16:29:22 +0100

    add some OASpectrum deployment stuff

---
+++ deploy_scripts/oaspectrum_master_deploy.sh
@@ -0,0 +1,22 @@
+sudo apt-get update -q -y
+sudo apt-get -q -y install libxml2-dev libxslt-dev python-dev lib32z1-dev
+cd /opt/oaspectrum
+. bin/activate
+cd src/oaspectrum
+git checkout master
+git pull
+git submodule update --init
+
+cd esprit
+git submodule update --init
+pip install -e .
+cd ..
+
+cd magnificent-octopus
+git submodule update --init
+pip install -e .
+cd ..
+
+pip install -e .
+
+sudo supervisorctl restart spectrum-test
