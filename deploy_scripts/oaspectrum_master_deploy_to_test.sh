d9671f6f6beeab75ec34b749281fad7ab888f920
Emanuil Tolev <emanuil.tolev@gmail.com>
2015-10-09 17:18:00 +0100

    fix OASpectrum test deploy

--- deploy_scripts/oaspectrum_master_deploy_to_test.sh
+++ deploy_scripts/oaspectrum_master_deploy_to_test.sh
@@ -1,8 +1,8 @@
 sudo apt-get update -q -y
 sudo apt-get -q -y install libxml2-dev libxslt-dev python-dev lib32z1-dev
 cd /opt/oaspectrum
-. bin/activate
-cd src/oaspectrum
+. env/bin/activate
+cd oaspectrum
 git checkout master
 git pull
 git submodule update --init
