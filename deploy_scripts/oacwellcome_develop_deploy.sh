d4b2671489a3934019b54f99e962c1abdf4ef178
Emanuil Tolev <emanuil.tolev@gmail.com>
2016-03-22 17:10:15 +0000

    basic temporary oacwellcome python configs

--- deploy_scripts/oacwellcome_develop_deploy.sh
+++ deploy_scripts/oacwellcome_develop_deploy.sh
@@ -1,22 +1,45 @@
+sudo ln -sf /opt/sysadmin/config/supervisor/conf.d/oacwellcome-test.conf /etc/supervisor/conf.d/oacwellcome-test.conf
+sudo ln -sf /opt/sysadmin/config/supervisor/conf.d/oagr-oacwellcome-test-daemon.conf /etc/supervisor/conf.d/oagr-oacwellcome-test-daemon.conf
+sudo ln -sf /opt/sysadmin/config/supervisor/conf.d/oacwellcome-test-daemon.conf /etc/supervisor/conf.d/oacwellcome-test-daemon.conf
+
+sudo ln -sf /opt/sysadmin/config/nginx/sites-available/oacwellcome-test /etc/nginx/sites-available/oacwellcome-test
+sudo ln -sf /etc/nginx/sites-available/oacwellcome-test /etc/nginx/sites-enabled/oacwellcome-test
+
 sudo apt-get update -q -y
 sudo apt-get -q -y install libxml2-dev libxslt-dev python-dev lib32z1-dev
+
+if [ ! -d /opt/oacwellcome ]; then
+    cd /opt
+    sudo mkdir oacwellcome
+    sudo chown cloo:cloo oacwellcome
+    virtualenv -p python2.7 oacwellcome
+    cd oacwellcome
+    mkdir src
+    cd src
+    git clone https://github.com/CottageLabs/oacwellcome.git oacwellcome
+fi
+
 cd /opt/oacwellcome
 . bin/activate
 cd src/oacwellcome
 git checkout develop
 git pull
-git submodule update --init
+git submodule update --recursive --init
+git submodule update --recursive
 
 cd esprit
-git submodule update --init
 pip install -e .
 cd ..
 
 cd magnificent-octopus
-git submodule update --init
 pip install -e .
 cd ..
 
 pip install -e .
 
+sudo supervisorctl reread
+sudo supervisorctl update
 sudo supervisorctl restart oacwellcome-test
+sudo supervisorctl restart oacwellcome-test-daemon
+sudo supervisorctl restart oagr-oacwellcome-test-daemon
+sudo nginx -t && sudo nginx -s reload
